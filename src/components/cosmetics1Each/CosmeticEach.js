0f7ad48fc28af42d55fa670e4affdb9bc1357ac7
Anamul haque  shanto <anamulhaque0198877@gmail.com>
2022-03-21 02:38:51 +0600

    added event hadler with parameter for cart

--- src/components/cosmetics1Each/CosmeticEach.js
+++ src/components/cosmetics1Each/CosmeticEach.js
@@ -1,15 +1,22 @@
 import React from 'react';
-
+import './CosmeticEach.css';
 const CosmeticEach = (props) => {
     console.log(props)
     const {name, price, id} = props.cosmetic;
+
+    const addToCart=(id)=>{
+        console.log("added",id);
+    }
+    const addCartWithParameter = ()=> addToCart(id);  //aikhane addCartWithParameter hocce wrapper
     return (
-        <div>
+        <div className='product'>
             {/* <h3>Buy this:{props.cosmetic.name} </h3>
             <p>only for: {props.cosmetic.price}</p> */}
             <h3>Buy this:{name} </h3>
             <p>only for: {price}</p>
             <p>id: {id}</p>
+            <button onClick = {addCartWithParameter}>add to cart</button>
+            <button onClick = {()=> addToCart(id)}>add to cart : shortcut</button>
         </div>
     );
 };
